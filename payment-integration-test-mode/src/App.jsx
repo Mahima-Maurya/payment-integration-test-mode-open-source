0c06fd8635ae4501bd18c911acb26a37bdbbb319
Mahima-Maurya <mahimaurya2004@gmail.com>
2025-09-04 23:02:29 +0530

    Feat:4/9/25:Created new project for payment integration test mode

---
+++ payment-integration-test-mode/src/App.jsx
@@ -0,0 +1,35 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+// import Donation from './frontend/Donation.jsx'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <Donation/>
+//   )
+// }
+
+// export default App
+
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Donation from "./frontend/Donation";
+
+
+const App = () => {
+  return (
+   <Router>
+    <Routes>
+      <Route path="/" element={<Donation/>}/>   
+    </Routes>
+   </Router>
+    
+  );
+};
+
+export default App;
+
---
+++ payment-integration-test-mode/src/frontend/Donation.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import "./Donation.css";
+
+const Donation = () => {
+  const [amount, setAmount] = useState(""); // custom amount state
+
+  // button click pe input box me value set karega
+  const handleDonationClick = (value) => {
+    setAmount(value);
+  };
+  return (
+    <section id="donate" className="section">
+      <div className="container">
+        <h2 className="section_title">Support Our Cause</h2>
+        <div className="fade_in">
+          <p className="donate_message">
+            Your contribution helps us continue our mission of creating positive
+            change in communities.
+          </p>
+
+          <div className="donate_options">
+            <button
+              className="donate_btn"
+              onClick={() => handleDonationClick(100)}
+            >
+              ₹100
+            </button>
+            <button
+              className="donate_btn"
+              onClick={() => handleDonationClick(500)}
+            >
+              ₹500
+            </button>
+            <button
+              className="donate_btn"
+              onClick={() => handleDonationClick(1000)}
+            >
+              ₹1000
+            </button>
+            <button
+              className="donate_btn"
+              onClick={() => handleDonationClick(2500)}
+            >
+              ₹2500
+            </button>
+          </div>
+
+          <div className="custom_amount">
+            <input
+              type="number"
+              id="customAmount"
+              placeholder="Enter custom amount"
+              min="1"
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+            />
+            <button
+              className="btn_primary"
+              onClick={() => alert(`Thanking You to donate ₹${amount} !!`)}
+            >
+              Donate Custom Amount
+            </button>
+          </div>
+
+          <div className="impactBox">
+            <h4 className="impactTitle">Your Impact</h4>
+            <div className="impactGrid">
+              <div className="impactItem">
+                <strong className="impactValue">₹100</strong>
+                <br />
+                <small className="impactText">Feeds 5 people</small>
+              </div>
+              <div className="impactItem">
+                <strong className="impactValue">₹500</strong>
+                <br />
+                <small className="impactText">Plants 10 trees</small>
+              </div>
+              <div className="impactItem">
+                <strong className="impactValue">₹1000</strong>
+                <br />
+                <small className="impactText">
+                  Provides clothing for a family
+                </small>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Donation;
